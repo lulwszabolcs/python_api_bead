5ab3d5ec1e452f6df61ceb9f7ac45875df7c5766
lulwszabolcs <szabolcs050318@gmail.com>
2026-04-06 19:46:03 +0200

    Validation complete

--- API/schemas/schema.py
+++ API/schemas/schema.py
@@ -19,20 +19,49 @@ Az osztályokban az adatok legyenek validálva.
 ShopName='Bolt'
 
 class Item(BaseModel):
-    item_id: int 
-    name: str 
+    item_id: int
+    @field_validator("item_id")
+    def check_item_id(cls,item_id):
+        if item_id < 0:
+            raise ValueError("Hiba: Az item_id nem lehet negatív!")
+        return item_id
+    
+    name: str
     brand: str
     price: float
+
     quantity: int
+    @field_validator("quantity")
+    def check_quantity(cls,quantity):
+        if quantity < 0:
+            raise ValueError("Hiba: A quantity nem lehet negatív!")
+        return quantity
 
 
 class Basket(BaseModel):
     id: int
+    @field_validator("id")
+    def check_id(cls,id):
+        if id < 0:
+            raise ValueError("Hiba: Az id (Kosar) nem lehet negatív!")
+        return id
+    
     user_id: int 
+    @field_validator("user_id")
+    def check_user_id(cls,user_id):
+        if user_id < 0:
+            raise ValueError("Hiba: Az user_id nem lehet negatív!")
+        return user_id
+    
     items: List[Item]
 
 
 class User(BaseModel):
     id: int
+    @field_validator("id")
+    def check_id(cls,id):
+        if id < 0:
+            raise ValueError("Hiba: Az id (Felhasznalo) nem lehet negatív!")
+        return id
     name: str
-    email: str
+    email: EmailStr
